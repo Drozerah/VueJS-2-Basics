5ddae0a96458a1ea17cce6ecac45e29ae5f9a153
Drozerah <drozerah@drozerah.user>
2018-09-06 19:24:43 +0200

    Update instruction

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 new Vue({
     el: '#app',
     data: {
+        title: "Dynamically bind attributes",
+        instruction: "Enter the \"blue\" or \"red\" string into the input text field below",
         cssClass: ''
     }
 
